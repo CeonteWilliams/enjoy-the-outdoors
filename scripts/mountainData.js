673489ea5cb7cb68fd7a9cafa63628a551f2f25b
Student <ceontewilliams820@gmail.com>
2023-11-11 11:14:37 -0500

    all saved items

---
+++ scripts/mountainData.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const mountainName = "mountains"
+let html = ""
+
+for (let mountainIndex = 0; mountainIndex < mountainsArray.length; mountainIndex += 1) {
+    const mountain =  mountainsArray[mountainIndex]
+    
+    html += `${mountainName[mountainIndex]}`}
+
+    
