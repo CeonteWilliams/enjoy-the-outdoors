04212755f3526cb0e6155784bbd7ec106743be91
Student <ceontewilliams820@gmail.com>
2023-11-13 15:09:13 -0500

    all saved changes

--- scripts/mountainData.js
+++ scripts/mountainData.js
@@ -1,11 +1,54 @@
 "use strict"
+window.onload = function (_event) {
 
-const mountainName = "mountains"
-let html = ""
 
-for (let mountainIndex = 0; mountainIndex < mountainsArray.length; mountainIndex += 1) {
-    const mountain =  mountainsArray[mountainIndex]
-    
-    html += `${mountainName[mountainIndex]}`}
+    const mountainSelect = document.getElementById("Mountains")
+    mountainSelect.onchange = renderactivitycard
 
-    
+    populateMountains(mountainsArray, mountainSelect)
+
+
+
+}
+
+function populateMountains(mountainsArray, selectElement) {
+    let html = "<option> Choose One </option>"
+    for (let index = 0; index < mountainsArray.length; index += 1) {
+        const currentMountain = mountainsArray[index];
+        html +=
+
+            `<option value="${currentMountain.name}">${currentMountain.name}</option>`
+    }
+
+
+    selectElement.innerHTML = html
+
+
+
+}
+
+
+function renderactivitycard(event) {
+    const activitycard = event.target.value
+    let html = ""
+    for (let index = 0; index < mountainsArray.length; index += 1) {
+        const mountain = mountainsArray[index];
+
+        if (mountain.name === activitycard) {
+            html += `
+                <div class="card">
+                  <h5 class="card-header">${mountain.name}</h5>
+                  <div class="card-body">
+                       <h5 class="card-title">Where? <em>${mountain.desc}</em></h5>
+                       <p class="card-text">${mountain.elevation}</p>
+                       <img src="/data/images/${mountain.img}" alt="image">
+                     
+                    </div>
+                </div>
+            `
+
+        }
+        const resultdiv = document.getElementById("selectedMountain")
+        resultdiv.innerHTML = html
+    }
+}
