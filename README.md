e4aac2309d802cb688d9514cf120a36047d4e3c0
Student <ceontewilliams820@gmail.com>
2023-11-16 10:59:11 -0500

    all saved changes

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # enjoy-the-outdoors
+
+This is a an outdoors adventure information website built using HTML,CSS, and Javascript to show information about different mountains and national parks. It contains a homepage, National Parks Search page, and Mountains Information page. The features in the website were implemented using the Onchange function in Java script. The designs in this website were implemented using the select tag in HTML. CSS was also involved in this project to structure the webpage and fit my divs in the webpage, and make it more attractive.
+
+Example of an interesting line of code:
+
+` function renderactivitycard(event) {
+    const activitycard = event.target.value
+    let html = ""
+    for (let index = 0; index < mountainsArray.length; index += 1) {
+        const mountain = mountainsArray[index];
+
+        if (mountain.name === activitycard) {
+            html += `
+                <div class="card">
+                  <h5 class="card-header">${mountain.name}</h5>
+                  <div class="card-body">
+                       <h5 class="card-title"> <em>${mountain.desc}</em></h5>
+                       <p class="card-text">${mountain.elevation}</p>
+                       <img src="/data/images/${mountain.img}" alt="image">
+                     
+                    </div>
+                </div>
+            `
+
+        }
+        ## ![registration page](images/Screenshot%202023-10-12%20230245.png)
+## ![home page](/data/images/adventure%20time%20home.png)
+## ![parks page](/data/images/adventure%20time%20park%20page.png)
+## ![mountains page](/data/images/adventure%20time%20mountains%20page.png)
+## ![]
--- scripts/parks.js
+++ scripts/parks.js
@@ -25,12 +25,12 @@ function populateLocations(locationsArray, selectElement) {
 
     selectElement.innerHTML = html
 }
-function populatetypes(parkTypesArray,selectElement){
+function populatetypes(parkTypesArray, selectElement) {
     let html = "<option> By Type </option>"
-    for (let index = 0; index < parkTypesArray.length; index+=1) {
+    for (let index = 0; index < parkTypesArray.length; index += 1) {
         const currenttype = parkTypesArray[index];
         html +=
-        `<option value= "${currenttype}">${currenttype}</option>`
+            `<option value= "${currenttype}">${currenttype}</option>`
     }
     selectElement.innerHTML = html
 }
@@ -40,49 +40,31 @@ function createactivitycard(event) {
     for (let index = 0; index < nationalParksArray.length; index += 1) {
         const location = nationalParksArray[index];
 
-        if (activitycard === location.State) {
+        if (location.State === activitycard) {
             html += `
                 <div class="card">
                   <h5 class="card-header">${location.LocationName}</h5>
                   <div class="card-body">
                        <h5 class="card-title"> <em>${location.Address}</em></h5>
                        <p class="card-text">${location.City}</p>
-                     
+                       <a href="#" class="btn btn-primary"> visit site </a>
                     </div>
                 </div>
             `
-
-
-        }
-        const resultdiv = document.getElementById("selectedlocation")
-        resultdiv.innerHTML = html
-    }
-}
-function createactivitycard(event) {
-    const activitycard = event.target.value
-    let html = ""
-    for (let index = 0; index < nationalParksArray.length; index += 1) {
-        const location = nationalParksArray[index];
-
-        if (activitycard === location.State) {
+        }  if (location.LocationName.includes(activitycard) === true) {
             html += `
-                <div class="card">
-                  <h5 class="card-header">${location.LocationName}</h5>
-                  <div class="card-body">
-                       <h5 class="card-title">${location.Address}</h5>
-                       <p class="card-text">${location.City}</p>
-                       <p class="card-text">${location.Visit}</p>
-                     
-                    </div>
+            <div class="card">
+             <h5 class="card-header">${location.LocationName}</h5>
+              <div class="card-body">
+                  <h5 class="card-title"> <em>${location.Address}</em></h5>
+                   <p class="card-text">${location.City}</p>
+                   <a href="#" class="btn btn-primary"> visit site </a>
                 </div>
-            `
-            if(location.LocationName.includes === true)  {
-                html+= `<h5 hello world</h5>`
-            }
-
-
-        }
-        const resultdiv = document.getElementById("selectedtype")
-        resultdiv.innerHTML = html
+            </div>
+        `
+        
+       }
+       const resultdiv = document.getElementById("selectedtype")
+       resultdiv.innerHTML = html
     }
 }
